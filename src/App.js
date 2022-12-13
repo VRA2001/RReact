55face8d027b1bc908c71fdf76047ffc47bdffa9
VRA2001 <90080448+VRA2001@users.noreply.github.com>
2022-12-13 08:58:51 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,44 +1,117 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
-function Welcome(props) {
-    const [value, setValue] = useState();
-    return <>
-           <br/> {props.name} {value ? value.target.value : ''}
-        <input type={'text'} onChange={setValue} />
-    </>;
+import React from "react";
+class Clock extends React.Component{
+    
+    constructor(props){
+        super(props);
+        this.state = {
+            date: props.date
+        }
+    }
+
+    componentDidMount(){
+        this.timerId = setInterval(() => {
+            this.tick();
+        }, 1000);
+    }
+    
+    componentWillUnmount(){
+        clearInterval(this.timerId);
+    }
+    tick(){
+        this.setState({
+            date: new Date()
+        });
+    }
+    
+    render(){
+        const {date} =this.state; //{date: 13.12.2022}
+        const min =date.getMinutes();
+        const second = date.getSeconds();
+        const hour = date.getHours();
+
+
+        if ((second % 5 ==0) || (second==0)) {
+            return  <div> 
+             {date.toLocaleDateString()} <br/>
+        {hour}:{min}:{second}
+                    <div className="circle"> <h4>Круг</h4></div>
+            
+              </div>;
+        } 
+        else {
+        return  <div>    
+        {/* {date.toLocaleTimeString()} */}
+        {date.toLocaleDateString()} <br/>
+        {hour}:{min}:{second}
+        <div className="circle2"> <h4>Круг</h4></div>
+      </div>;
+        }
+    }
 }
-function App() {
-    return (
-        <div>
-            <Welcome name="ФИО:" /> <br/>
-            <Welcome name="Номер телефона:" /><br />
-            <Welcome name="E-mail:" /><br />
-            <Welcome name="Дата рождения:" /><br />
-            <Welcome name="Биографические сведения:" /><br />
-        </div>   
-    );
-}
-export default Welcome; 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+export default Clock;
 
-//import logo from './logo.svg';
-//import './App.css';
-//import Remarkable from 'remarkable';
-//import RemarkableReactRenderer from 'remarkable-react';
+//import React, { useState } from 'react';
+//import Clock from './components/Clock';
+//function Welcome(props) {
+    //const [value, setValue] = useState();
+    //return <>
+           //<br/> {props.name} {value ? value.target.value : ''}
+        //<input type={'text'} onChange={setValue} />
+    //</>;
+//}
+//function App() {
+    //return (
+        //<div>
+            //<Welcome name="ФИО:" /> <br/>
+            //<Welcome name="Номер телефона:" /><br />
+            //<Welcome name="E-mail:" /><br />
+            // <Welcome name="Дата рождения:" /><br />
+            // <Welcome name="Биографические сведения:" /><br />
+        //</div>   
+    //);
+//}
+//export default Welcome; 
+//const root = ReactDOM.createRoot(document.getElementById('root'));
 
-//class MarkdownEditor extends React.Component {
-  //constructor(props) {
-    //super(props);
-    //this.md = new Remarkable();
-    //this.handleChange = this.handleChange.bind(this);
-    //this.state = { value: 'Привет, **мир**!' };
-  //}
+//function App() {
+  //const timer = function() {
+  // {new Date().toDateString()}
+  // {new Date().toLocaleDateString()}
+  //return 
+  //};
 
-  //handleChange(e) {
-    //this.setState({ value: e.target.value });
-  //}
-  //getRawMarkup() {
-    //return { __html: this.md.render(this.state.value) };
-  //}
-       
+    //return 
+    //<Clock date={new Date()}/>
+    //<Clock date={new Date()}/>
+    //<Clock date={new Date()}/>
+    //<Clock date={new Date()}/>
+    //<Clock date={new Date()}/>
+//}
+
+//export default App;
+
+
+//import React, {useState} from 'react';
+
+//function App(){
+
+  // let a = 1;
+  // let b = 9;
+  // let c = a+b;
+  // b +=a + b;
+  // console.log(c);
+
+  //const [value, setValue] = useState();
+
+//console.log(value);
+
+   //return <>
+      //<p> 
+        //{value ? value.target.value : ""}
+      //</p>
+
+      //<input/ type={"text"} onChange={setValue}/>
+   //</>;
+//}
+
+//export default App;
